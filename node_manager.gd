4013a1becd978c3335804be066e063547eba2403
FyreDay <fyredaymc@gmail.com>
2026-08-13 04:17:33 -0500

    Made regions mergable and added merging, coloring, and name changes to under/redo

--- node_manager.gd
+++ node_manager.gd
@@ -7,6 +7,8 @@ var region_scene = preload("res://region.tscn")
 var isDrawingRegion = false
 var dragStartMousePos: Vector2
 var dragSizeVector: Vector2
+var isMerging = false
+var mergingRegion = null
 
 var undo_redo := UndoRedo.new()
 # Called when the node enters the scene tree for the first time.
@@ -34,8 +36,11 @@ func _draw() -> void:
 
 func draw_region(delta):
     if !isDrawingRegion and Input.is_action_just_pressed("draw_region"):
+        if not Input.is_key_pressed(KEY_SHIFT):
+            return
         dragStartMousePos = get_global_mouse_position()
         isDrawingRegion = true
+        isMerging = false
         
     if isDrawingRegion and Input.is_action_just_released("draw_region"):
         isDrawingRegion = false
@@ -51,6 +56,11 @@ func create_region(rect: Rect2):
     region.setup(rect)
     region.popup_opened.connect(_on_region_popup_opened)
     region.popup_closed.connect(_on_region_popup_closed)
+    region.delete_region.connect(_on_delete_region)
+    region.merge_start.connect(_on_merge_start)
+    region.clicked_region.connect(_on_region_clicked)
+    region.name_change_request.connect(_on_region_name_change_requested)
+    region.color_change_request.connect(_on_region_color_change_requested)
     undo_redo.create_action("Create Region")
     undo_redo.add_do_method(add_child.bind(region))
     undo_redo.add_undo_method(remove_child.bind(region))
@@ -61,7 +71,23 @@ func draw_entrance(delta):
         
 func create_entrance(delta):
     pass
-    
+func do_region_merge(region, new_parent_region):
+    region.do_merge(new_parent_region)
+
+func restore_regions(snapshot):
+    for region in snapshot:
+        var state = snapshot[region]
+
+        region.is_merge_controller = state.is_merge_controller
+        region.merge_controller = state.merge_controller
+        region.region_references = state.region_references.duplicate()
+        region.region_name = state.region_name
+        region.region_color = state.region_color
+        region.queue_redraw()
+        
+func undo_region_merge(region_state1, region_state2):
+    restore_regions(region_state1)
+    restore_regions(region_state2)
 
 
 func _on_region_popup_opened() -> void:
@@ -70,4 +96,53 @@ func _on_region_popup_opened() -> void:
 
 func _on_region_popup_closed() -> void:
     region_popup_closed.emit()
+
+func _on_delete_region(region) -> void:
+    undo_redo.create_action("Delete Region")
+    undo_redo.add_do_method(delete_region_and_reference.bind(region))
+    undo_redo.add_undo_method(redo_region_and_reference.bind(region))
+    undo_redo.commit_action()
+
+func delete_region_and_reference(region):
+    if region.merge_controller != null:
+        region.merge_controller.remove_region_reference(region) 
+    remove_child.bind(region)
     
+func redo_region_and_reference(region):
+    if region.merge_controller != null:
+        region.merge_controller.add_region_reference(region) 
+    add_child.bind(region)
+
+func _on_merge_start(region):
+    isMerging = true
+    mergingRegion = region
+    
+    
+func _on_region_clicked(region):
+    if isMerging:
+        if region.is_merge_valid(mergingRegion):
+            undo_redo.create_action("Merge Region")
+            undo_redo.add_do_method(do_region_merge.bind(region, mergingRegion))
+            undo_redo.add_undo_method(undo_region_merge.bind(region.snapshot_regions(), mergingRegion.snapshot_regions()))
+            undo_redo.commit_action()
+        isMerging = false
+        
+func _on_region_name_change_requested(region, new_name):
+    var old_name = region.region_name
+
+    undo_redo.create_action("Change Region Name")
+
+    undo_redo.add_do_method(region.set_region_name.bind(new_name))
+    undo_redo.add_undo_method(region.set_region_name.bind(old_name))
+
+    undo_redo.commit_action()
+func _on_region_color_change_requested(region, new_color, old_color):
+
+    undo_redo.create_action("Change Region Color")
+
+    undo_redo.add_do_method(region.set_region_color.bind(new_color))
+    undo_redo.add_undo_method(region.set_region_color.bind(old_color))
+
+    undo_redo.commit_action()
+        
+        
--- region.gd
+++ region.gd
@@ -2,15 +2,25 @@ extends Node2D
 
 signal popup_opened
 signal popup_closed
+signal delete_region
+signal merge_start
+signal clicked_region
+signal name_change_request
+signal color_change_request
 
 @onready var edit_menu: PopupPanel = $EditMenu
 @onready var name_edit: LineEdit = $EditMenu/VBoxContainer/NameEdit
 @onready var color_changer: ColorPickerButton = $EditMenu/VBoxContainer/ColorChanger
 
 var region_color := Color.WHITE
+var old_region_color
 var node_rect := Rect2(Vector2.ZERO,Vector2(100,100))
 var region_name = ""
 const alpha = .3
+
+var merge_controller
+var region_references
+var is_merge_controller
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     pass # Replace with function body.
@@ -21,9 +31,12 @@ func _process(delta: float) -> void:
     pass
 
 func setup(rect: Rect2):
-    node_rect = rect
+    node_rect = rect.abs()
     region_color = Color.from_hsv(randf(), 1.0, 1.0,alpha)
     show_behind_parent = true
+    merge_controller = null
+    region_references = []
+    is_merge_controller = true
 
 func _draw() -> void:
     var font := ThemeDB.fallback_font
@@ -43,8 +56,16 @@ func _input(event: InputEvent) -> void:
 
         if node_rect.has_point(to_local(mouse_pos)):
             open_edit_menu()
+    if event.is_action_pressed("click"):
+        var mouse_pos := get_global_mouse_position()
+
+        if node_rect.has_point(to_local(mouse_pos)):
+            clicked_region.emit(self)
 
 func open_edit_menu():
+    if not is_merge_controller:
+        merge_controller.open_edit_menu()
+        return
     popup_opened.emit()
     name_edit.text = region_name
     color_changer.color = Color(region_color, 1.0)
@@ -53,6 +74,66 @@ func open_edit_menu():
     edit_menu.reset_size()
     edit_menu.popup()
 
+func is_merge_valid(new_merge_controller):
+    if self == new_merge_controller:
+        return false
+    if merge_controller != null and new_merge_controller.merge_controller != null:
+        if merge_controller == new_merge_controller.merge_controller:
+            return false
+    if merge_controller == new_merge_controller:
+        return false
+    if is_merge_controller and new_merge_controller.merge_controller == self:
+        return false
+    return true
+        
+        
+func get_merge_state():
+    return {
+        "is_merge_controller": is_merge_controller,
+        "merge_controller": merge_controller,
+        "region_references": region_references.duplicate(),
+        "region_name": region_name,
+        "region_color": region_color,
+    }
+    
+func snapshot_regions():
+    var snapshot = {}
+
+    for region in region_references:
+        snapshot[region] = region.get_merge_state()
+    snapshot[self] = self.get_merge_state()
+    if merge_controller != null:
+        snapshot[merge_controller] = merge_controller.get_merge_state()
+    return snapshot
+    
+func do_merge(new_merge_controller):
+    if not is_merge_valid(new_merge_controller):
+        return
+    if is_merge_controller and region_references.size() > 0:
+        var new_parent_region = region_references[0]
+        new_parent_region.merge_controller = null
+        new_parent_region.is_merge_controller = true
+        new_parent_region.region_references = region_references.slice(1)
+        for new_child in new_parent_region.region_references:
+            new_child.merge_controller = new_parent_region
+        region_references = []
+    is_merge_controller = false
+    if merge_controller != null:
+        merge_controller.remove_region_reference(self)
+    merge_controller = new_merge_controller
+    region_name = merge_controller.region_name
+    region_color = merge_controller.region_color
+    merge_controller.add_region_reference(self)
+    
+    queue_redraw()
+    
+    
+func add_region_reference(region):
+    region_references.append(region)
+    
+func remove_region_reference(region):
+    region_references.erase(region)
+    
 
 func _on_edit_menu_popup_hide() -> void:
     popup_closed.emit()
@@ -60,13 +141,44 @@ func _on_edit_menu_popup_hide() -> void:
 
 func _on_color_changer_color_changed(color: Color) -> void:
     region_color = Color(color, alpha)
-    queue_redraw()
+    set_region_color(region_color)
+
 
 
 func _on_name_edit_text_changed(new_text: String) -> void:
-    region_name = new_text
+    name_change_request.emit(self, new_text)
+    
+func set_region_name(new_name: String) -> void:
+    for region in region_references:
+        region.set_region_name(new_name)
+
+    region_name = new_name
+    queue_redraw()
+    
+func set_region_color(new_color: Color) -> void:
+    if is_merge_controller:
+        for region in region_references:
+            region.set_region_color(new_color)
+
+    region_color = Color(new_color, alpha)
     queue_redraw()
 
 
 func _on_delete_button_pressed() -> void:
-    queue_free()
+    edit_menu.hide()
+    delete_region.emit(self)
+    
+func _on_merge_button_pressed() -> void:
+    if is_merge_controller:
+        merge_start.emit(self)
+    else:
+        merge_start.emit(merge_controller)
+        
+
+
+func _on_color_changer_popup_closed() -> void:
+    color_change_request.emit(self, region_color, old_region_color)
+
+
+func _on_color_changer_picker_created() -> void:
+    old_region_color = region_color
